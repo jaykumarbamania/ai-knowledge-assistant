7b48f8a7c717be5ae80434e654b4d0b181c2402a
jaybamania <90679000+jaykumarbamania@users.noreply.github.com>
2026-02-28 19:47:40 +0530

    Updated to Dynamic response

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+user_input = input("Ask something: ")
+
 response = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
         {"role": "system", "content": "You are a helpful AI assistant."},
-        {"role": "user", "content": "Explain what is an LLM in simple terms."}
+        {"role": "user", "content": user_input}
     ],
     temperature=0.7
 )
